7dbff37f420afa4f738348511b008e7929a2681d
Yahia-Baj <yahia.bajbouj@tradinos.com>
2022-07-29 02:18:36 +0300

    set-up

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Contestant;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $contestants = Contestant::all();
+        return view('index', compact('contestants'));
+    }
+
+    public function vote(Request $request){
+        Contestant::find($request->contestantId)->increment('voite_count');
+        $request->session()->put('voted', true);
+        return back();
+    }
+}
